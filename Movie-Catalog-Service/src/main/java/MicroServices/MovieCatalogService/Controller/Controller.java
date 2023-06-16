d5f8a6f37de70cf676f44bb171263c6ac88208ab
Sai Kiran <kirann9679@icloud.com>
2023-06-16 14:08:39 -0500

    new files

--- Movie-Catalog-Service/src/main/java/MicroServices/MovieCatalogService/Controller/Controller.java
+++ Movie-Catalog-Service/src/main/java/MicroServices/MovieCatalogService/Controller/Controller.java
@@ -8,6 +8,7 @@ import MicroServices.MovieCatalogService.Service.MovieInfo;
 import MicroServices.MovieCatalogService.Service.UserRatingInfo;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.stereotype.Component;
@@ -28,17 +29,25 @@ public class Controller {
     RestTemplate restTemplate;
     @Autowired
     WebClient webClient;
+    Log4j logger;
     @Autowired
     MovieInfo movieInfo;
     @Autowired
     UserRatingInfo userRatingInfo;
+    @Autowired
+    WebClient web;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getcatalog(@PathVariable("userId") String userId) {
 
         UserRating ratings = userRatingInfo.getUserRating(userId);
-        return ratings.getUserRating().stream().map(r -> movieInfo.getCatalogItem(r))
+
+        List<CatalogItem> response = ratings.getUserRating().stream().map(r -> movieInfo.getCatalogItem(r))
                 .collect(Collectors.toList());
+
+
+
+        return response;
     }
 }
 
